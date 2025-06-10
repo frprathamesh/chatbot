3ce04418b32bb263cbd3705afe89668d98d2db5d
frprathamesh <patilprathamesh562@gmail.com>
2025-06-10 21:27:29 +0530

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_PASS', '');
 define('DB_NAME', 'college_helpdesk');
 
 function getDBConnection() {
-    $conn = new mysqli('sql203.infinityfree.com', 'epiz_XXXXXXX', 'prat1019', 'if0_39199339_college_helpdesk');
+$conn = new mysqli('sql203.infinityfree.com', 'if0_39199339', 'prat1019', 'if0_39199339_college_helpdesk');
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
